8463944c8857e58e4fb0d1515a9d032f9ec5e70f
gjyyy <47134614+gjyyy@users.noreply.github.com>
2020-03-25 16:36:27 +0800

    Add files via upload

---
+++ components/public/footer.vue
@@ -0,0 +1,75 @@
+<template lang="html">
+    <Panel title="底部" class="footer-panel">
+        <ul class="footer-content">
+            <li>
+                <img src="//img12.360buyimg.com/jrpmobile/jfs/t2842/350/3035567089/14791/5f6ff93d/577cf395N31e76288.png?width=1125&height=252" alt="">
+            </li>
+            <li>
+                <div>
+                    <img src="//img12.360buyimg.com/jrpmobile/jfs/t2971/333/1297567079/898/f2d2e00d/577dc28dNe5138337.png?width=108&height=108" alt="">
+                    <p>客户版</p>
+                </div>
+                <div>
+                    <img src="//img12.360buyimg.com/jrpmobile/jfs/t2824/256/2966087355/831/188bfa25/577cf3dcN18aadbf2.png?width=108&height=108" alt="">
+                    <p>触屏版</p>
+                </div>
+                <div>
+                    <img src="//img12.360buyimg.com/jrpmobile/jfs/t2920/282/1283157010/1040/23f1430b/577cf3e5N53f740b8.png?width=108&height=108" alt="">
+                    <p>电脑版</p>
+                </div>
+            </li>
+        </ul>
+
+    </Panel>
+    
+</template>
+<script>
+import Panel from "@/components/core/panel.vue"
+export default {
+    components:{
+        Panel,
+    }
+    
+}
+</script>
+<style lang="scss">
+@import "../../css/element.scss";
+.footer-panel{
+    @include panel;
+    margin-bottom: 90px;
+    &>h4{
+        display: none;
+    }
+    .footer-content{
+        @include flex;
+        background: #F5F5F5;
+        li{
+            width:100%;
+            text-align: center;
+            font-size: 24px;
+            color:#999;
+            >img{
+                width: 100%;
+                height: 143px;
+            }
+            &:nth-child(2){
+                @include flex(row);
+                height: 186px;
+                box-sizing: border-box;
+                >div{
+                    width: 33.33%;
+                    color:#999;
+                    margin-top:12px;
+                    white-space: nowrap;
+                    text-overflow: ellipsis;
+                    overflow: hidden;
+                }
+            }
+        }
+    }
+
+}
+
+</style>
+
+
---
+++ components/public/navbar.vue
@@ -0,0 +1,77 @@
+<template lang="html">
+    <Panel title="导航栏" class="panel1">
+        <ul class="content">
+            <li  @click="goto('/indexhome')">
+            <img src="//img12.360buyimg.com/jrpmobile/jfs/t3811/55/345433999/2930/116d3dd1/580f1e25N028a1f2c.png?width=66&height=66" alt="">
+<p>首页</p></li>
+            <li  @click="goto('/indexious')">
+            <img src="//img12.360buyimg.com/jrpmobile/jfs/t3343/179/621044750/1484/5af6b11a/580f50e4N005181d0.png?width=66&height=66" alt="">
+<p>白条</p></li>
+            <li  @click="goto('/indexraise')">
+            <img src="//img12.360buyimg.com/jrpmobile/jfs/t3604/98/635506544/2317/443d3a51/580f240aNbee3dcc4.png?width=66&height=66" alt="">
+<p>众筹</p></li>
+            <li  @click="goto('/indexmoney')">
+            <img src="//img12.360buyimg.com/jrpmobile/jfs/t3340/68/639059082/2591/800e2d6b/580f51cdN4caf20b2.png?width=66&height=66" alt="">
+<p>理财</p></li>
+            <li >
+                <img src="//img12.360buyimg.com/jrpmobile/jfs/t3340/68/639059082/2591/800e2d6b/580f51cdN4caf20b2.png?width=66&height=66" alt="">
+<p>我的<p/></li>
+        </ul>
+
+    </Panel>
+</template>
+<script>
+import Panel from "@/components/core/panel.vue"
+export default {
+    components:{
+        Panel,
+    },
+
+    methods:{
+        goto(path){
+            this.$router.push(path)
+        }
+    }    
+}
+</script>
+<style lang="scss">
+@import "@/css/element.scss";
+  .panel1{
+    @include panel;
+    >h4{
+        display: none;
+    }
+    position: fixed;
+    z-index: 100;
+    left:0;
+    right: 0;
+    bottom: 0;
+    height: 100px;
+    margin: 0;
+    .content{
+      @include flex(row);
+      background-color: #fff;
+      justify-content: space-around;
+      li{
+        text-align: center;
+        a{
+          text-decoration: none;
+        }
+        img{
+          width: 44px;
+          height: 44px;
+          display: inline-block;
+          margin: 12px auto 6px;
+        }
+        p{
+          font-size: 22px;
+          color: #656565;
+        }
+      }
+    }
+  }
+
+
+</style>
+
+
